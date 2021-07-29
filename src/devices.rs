e34b8a401cdc4e227dd4833290383bb9ae3fee76
ftilde <ftilde@tamepointer.de>
2021-07-29 22:12:41 +0200

    Add verified status to devices command

--- src/devices.rs
+++ src/devices.rs
@@ -6,13 +6,21 @@ pub async fn run(client: Client) -> Result<(), matrix_sdk::Error> {
         .await
         .expect("Can't get devices from server");
 
-    println!("ID\tDevice name",);
+    println!("ID\tDevice name\tverified",);
 
+    let user_id = client.user_id().await.unwrap();
     for device in response.devices {
+        let crypt_device = client
+            .get_device(&user_id, &*device.device_id)
+            .await
+            .unwrap();
         println!(
-            "{}\t{}",
+            "{}\t{}\t{}",
             device.device_id,
-            device.display_name.as_deref().unwrap_or("")
+            device.display_name.as_deref().unwrap_or(""),
+            crypt_device
+                .map(|d| d.verified().to_string())
+                .unwrap_or("unknown".to_owned())
         );
     }
 
